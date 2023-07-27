63e90321ccc516302a5aa84e1e2cdb900a780f3e
Andrei-Cristian S <andrei.sandor@outlook.com>
2023-07-27 12:33:50 +0300

    Add Profiles for AutoMapper

---
+++ ticket-management/ticket-management/Profiles/EventProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ticket_management.Models;
+using ticket_management.Models.Dto;
+
+namespace ticket_management.Profiles
+{
+	public class EventProfile : Profile
+	{
+        public EventProfile()
+        {
+            CreateMap<Event, EventDTO>().ReverseMap();
+            CreateMap<Event, EventPatchDTO>().ReverseMap();
+        }
+    }
+}
---
+++ ticket-management/ticket-management/Profiles/OrderProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ticket_management.Models;
+using ticket_management.Models.Dto;
+
+namespace ticket_management.Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, OrderDTO>().ReverseMap();
+            CreateMap<Order, OrderPatchDTO>().ReverseMap();
+        }
+    }
+}
