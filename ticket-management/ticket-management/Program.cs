dc1cd126b54a5d2eb3be618141fd3ef8ea2efead
Andrei-Cristian S <andrei.sandor@outlook.com>
2023-07-27 15:54:06 +0300

    Update Program.cs & project files

--- ticket-management/ticket-management/Program.cs
+++ ticket-management/ticket-management/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IEventRepository, EventRepository>();
 builder.Services.AddTransient<IOrderRepository, OrderRepository>();
 builder.Services.AddTransient<ITicketCategoryRepository, TicketCategoryRepository>();
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
