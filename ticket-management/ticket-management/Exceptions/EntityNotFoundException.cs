0474d40f73ba926d1ce3b8003070dcaced852451
Andrei-Cristian S <andrei.sandor@outlook.com>
2023-07-31 16:02:29 +0300

    Add EntityNotFound exception

---
+++ ticket-management/ticket-management/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace ticket_management.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException() { }
+
+        public EntityNotFoundException(string merrorMessage) : base(merrorMessage) { }
+
+        public EntityNotFoundException(string merrorMessage, Exception innerException) : base(merrorMessage, innerException) { }
+
+        public EntityNotFoundException(long entityId, string entityName) : base(FormattableString.Invariant($"'{entityName}' with ID '{entityId}' was not found.")) { }
+
+        public EntityNotFoundException(string name, string entityName) : base(FormattableString.Invariant($"'{entityName}' with name '{name}' was not found.")) { }
+    }
+}
